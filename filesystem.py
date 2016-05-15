1a7a97c2897879c9b498665dff0d1518d2224660
IrinaNizova <alisher_inn@mail.ru>
2016-05-15 21:10:10 +0300

    add Directory class and rename test file

--- filesystem.py
+++ filesystem.py
@@ -3,12 +3,21 @@ class File:
         self.name = kwargs['name']
 
     def __str__(self):
-        return self.name
+        return 'file ' + self.name
+
+class Directory(dict):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.name = kwargs['name']
+
+    def __str__(self):
+        return 'directory '
 
 
 class Filesystem:
 
-    types = {dict: 'directory', File: 'file'}
+    types = {Directory: 'directory', File: 'file'}
 
     def __init__(self):
         self.filesystem = {}
@@ -23,10 +32,10 @@ class Filesystem:
             raise ValueError('Path is not valid')
         if name in parent_dir.keys():
             raise KeyError('This {} has already exists'.format(self.types[el_type]))
-        parent_dir[name] = {} if el_type == dict else File(name=name)
+        parent_dir[name] = Directory(name=name) if el_type == Directory else File(name=name)
 
     def create_dir(self, path, name):
-        return self._create_element(path, name, dict)
+        return self._create_element(path, name, Directory)
 
     def create_file(self, path, name):
         return self._create_element(path, name, File)
@@ -50,7 +59,7 @@ class Filesystem:
             raise ValueError('This {} does not exist'.format(self.types[el_type]))
         if not isinstance(parent_dir[name], el_type):
             raise TypeError('This is not {}'.format(self.types[el_type]))
-        if el_type == dict and len(parent_dir[name]) > 0:
+        if el_type == Directory and len(parent_dir[name]) > 0:
             raise ValueError('This {} does not empty'.format(self.types[el_type]))
         del parent_dir[name]
 
@@ -58,7 +67,7 @@ class Filesystem:
         return self._delete_element(path, name, File)
 
     def delete_dir(self, path, name):
-        return self._delete_element(path, name, dict)
+        return self._delete_element(path, name, Directory)
 
     def _get_reference_for_el(self, path):
         filesystem = self.filesystem
